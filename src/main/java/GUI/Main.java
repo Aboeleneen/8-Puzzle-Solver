d387a7025afbf41358b4bb8d94769ff1ea5d7d64
Aboeleneen <aboeleneen.98@yahoo.com>
2020-11-16 12:36:35 +0200

    Implment BFS Solver

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -3,17 +3,21 @@ package GUI;
 import DFS_solver.DFSSolver;
 import DFS_solver.State;
 
+import BFS_Solver.BFSSolver;
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
         String tiles = "012345678";
-        DFSSolver solver = new DFSSolver(new State(tiles));
+        BFSSolver solver = new BFSSolver(new State(tiles));
         String initial_tiles = "125340678";
-        State reached_state = solver.solve(new State(initial_tiles));
-        int statesNum = 0;
-        while (reached_state.prevState != null) {
-            statesNum++;
-            reached_state = reached_state.prevState;
+        solver.solve(new State(initial_tiles));
+        Stack<State> moves = solver.get_moves();
+        System.out.println("Path cost: " + (moves.size() - 1));
+        while(!moves.isEmpty()){
+            State s = moves.pop();
+            System.out.println(s.tiles);
         }
-        System.out.println("Path cost: " + (statesNum - 1));
+        
     }
 }
