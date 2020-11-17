4fbfe944b439ba95c43922631a7c5745d00efb8f
Mahmoud Sharshar <mahmoudsharshar3@gmail.com>
2020-11-17 04:41:19 +0200

    Change maven configuration file (pom) to execute javafx

--- src/main/java/GUI/SolutionController.java
+++ src/main/java/GUI/SolutionController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package solver;
+package GUI;
 
 import DFS_solver.State;
 import java.io.IOException;
